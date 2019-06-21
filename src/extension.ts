ff75841af91d861c7a16d47a55a92c177a7f5fa1
Ruslan Kazakov <ruslan.kazakov@gmail.com>
2019-06-21 13:10:55 +1000

    Change PascalCase to Pascal_Snake_Case

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,18 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let disposable = vscode.commands.registerCommand('extension.pascal-snake-casify', () => {
+		const editor: any = vscode.window.activeTextEditor;
+		const text = editor.document.getText(editor.selection);
+		const words: any = text.match(/[\d\.]+|\D+/g);
+		let result: any = [];
+		words.forEach((word: any) => {
+			result = result.concat(word.split(/(?=[A-Z])/));
+		});
+		editor.edit((editBuilder: any) => {
+			const selection = editor.selection;
+			editBuilder.replace(selection, result.join('_'));
+		});
 	});
 
 	context.subscriptions.push(disposable);
